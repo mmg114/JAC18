b75ca9e2e21b77a6919c5383c4694e8b992fd81c
“mmg114” <mmg.usb@gmail.com>
2023-11-19 09:14:34 -0500

    first commit

---
+++ src/main/java/co/com/ps/JAC18/Jac18Application.java
@@ -0,0 +1,14 @@
+package co.com.ps.JAC18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jac18Application {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(Jac18Application.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/ps/JAC18/Jac18ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.ps.JAC18;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jac18ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
