31ba37bbb0f0b81b457f0b9582b594c83b3b630b
“mmg114” <mmg.usb@gmail.com>
2023-11-21 21:18:01 -0500

    Guardado de base de datos

--- src/main/java/co/com/ps/JAC18/controller/EventoController.java
+++ src/main/java/co/com/ps/JAC18/controller/EventoController.java
@@ -3,6 +3,7 @@ package co.com.ps.JAC18.controller;
 import co.com.ps.JAC18.modelo.Evento;
 import co.com.ps.JAC18.repository.EventoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,11 @@ public class EventoController {
         eventoRepository.save(evento);
 
         return "Evento lanzado exitosamente";
+    }
+
+    @GetMapping()
+    public String ping(){
+       return "ok";
     }
 
 
